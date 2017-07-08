1feb5e86acefb9d3c44fc80ba91e7ce4ae02fc55
Aditya <adityaaditya473473@gmail.com>
2017-07-09 03:46:40 +0530

    Added more functions and minor logic changes.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_user_id(user_name):
         print("Response code other than 200 received!!")
 
 # Method to Display the user info, let's you choose b/w self info and info using User ID
-def get_info():
+def get_info(user_name):
     ans=raw_input("1. Fetch your own Instagram information.\n2. Fetch someone else's Insatagram information.")
     if ans == '1':
         request_url = BASE_URL + "users/self/?access_token=%s" %(APP_ACCESS_TOKEN)
@@ -34,7 +34,7 @@ def get_info():
                 print("Follows : %s") % (user_info["data"]["counts"]["follows"])
                 print("Number of posts : %s") % (user_info["data"]["counts"]["media"])
             else:
-                return None
+                exit()
         else:
             print("Response code other than 200 received!!")
     elif ans == '2' :
@@ -59,10 +59,9 @@ def get_info():
     else:
         print("Please select correct Input!!")
 
-
 # Method to retrieve the recent posts , let's you choose b/w your posts or using different User ID and returns the post ID
 # and returns none if there is no data.
-def get_posts():
+def get_posts(user_name):
     ans = raw_input("1. Fetch your own Media posts.\n2. Fetch someone else's Media posts.")
     if ans == '1':
         user_info = requests.get(BASE_URL + ("users/self/media/recent/?access_token=%s") %(APP_ACCESS_TOKEN)).json()
@@ -87,36 +86,40 @@ def get_posts():
                     print("This type of media isn't supported yet!! Stay Tuned for more.")
                     exit()
             else:
-                return None
+                exit()
 
         else:
             print("Response code other than 200 received!!")
     elif ans == '2':
-        usr_id = str(get_user_id(user_name))
-        user_info = requests.get((BASE_URL + "users/%s/media/recent/?access_token=%s") %(usr_id , APP_ACCESS_TOKEN)).json()
-        if user_info["meta"]["code"] is 200:
-            if len(user_info["data"]) :
-                if user_info["data"][0]["type"] == "carousel":
-                    i = len(user_info["data"][0]["carousel_media"])
-                    index = 0
-                    for index in range(i):
-                        img_url = user_info["data"][0]["carousel_media"][index]["images"]["standard_resolution"]["url"]
-                        img_name = str(user_info["data"][0]["id"]) + str(index+1) + '.jpeg'
-                        urllib.urlretrieve(img_url,img_name)
-                    return user_info["data"][0]["id"]
-                elif user_info["data"][0]["type"] == "image":
-                    img_url = user_info["data"][0]["images"]["standard_resolution"]["url"]
-                    img_name = str(user_info["data"][0]["id"]) + '.jpeg'
-                    urllib.urlretrieve(img_url,img_name)
-                    return user_info["data"][0]["id"]
-                else:
-                    print("This type of media isn't supported yet!! Stay Tuned for more.")
-                    exit()
-            else:
-                return None
-
+        usr_id = get_user_id(user_name)
+        if usr_id is None:
+            print ("Sorry, the user name was not found!!")
         else:
-            print("Response code other than 200 received!!")
+            usr_id = str(usr_id)
+            user_info = requests.get((BASE_URL + "users/%s/media/recent/?access_token=%s") %(usr_id , APP_ACCESS_TOKEN)).json()
+            if user_info["meta"]["code"] is 200:
+                if len(user_info["data"]) :
+                    if user_info["data"][0]["type"] == "carousel":
+                        i = len(user_info["data"][0]["carousel_media"])
+                        index = 0
+                        for index in range(i):
+                            img_url = user_info["data"][0]["carousel_media"][index]["images"]["standard_resolution"]["url"]
+                            img_name = str(user_info["data"][0]["id"]) + str(index+1) + '.jpeg'
+                            urllib.urlretrieve(img_url,img_name)
+                        return user_info["data"][0]["id"]
+                    elif user_info["data"][0]["type"] == "image":
+                        img_url = user_info["data"][0]["images"]["standard_resolution"]["url"]
+                        img_name = str(user_info["data"][0]["id"]) + '.jpeg'
+                        urllib.urlretrieve(img_url,img_name)
+                        return user_info["data"][0]["id"]
+                    else:
+                        print("This type of media isn't supported yet!! Stay Tuned for more.")
+                        exit()
+                else:
+                    return None
+
+            else:
+                print("Response code other than 200 received!!")
 
     else:
         print("Please select correct Input!!")
@@ -131,11 +134,46 @@ def recent_liked_media():
     else:
         print("Response Code other than 200 recieved!!")
 
+# Method to post a like on a media accepting the username of the user.
+def post_like(user_name):
+    media_id = get_posts(user_name)
+    if media_id == None:
+        print ("Username not found!!")
+    else:
+        url = BASE_URL +("media/%s/likes") %(media_id)
+        payload = {"access_token":APP_ACCESS_TOKEN}
+        like_info = requests.post(url , payload).json()
+        if like_info["meta"]["code"] is 200:
+            print("Your like successfully posted.")
+        else:
+            print("Like was not posted due to some error.")
 
+# Method to get a list of all the comments on a user's post
+def get_comments(user_name):
+    usr_id = get_posts(user_name)
+    comment_info = requests.get(BASE_URL+ ("media/%s/comments?access_token=%s") %(usr_id , APP_ACCESS_TOKEN)).json()
+    if comment_info["meta"]["code"] is 200:
+        if comment_info["data"]:
+            no_of_comm = len(comment_info["data"])
+            print("List of comments on the post :")
+            for index in range(no_of_comm):
+                print("%s. %s") %(str(index+1) , comment_info["data"][index]["text"])
+        else:
+            print("No comment was found !!")
+    else:
+        print("Response code other than 200 found!!")
 
-
-
-
+# Method to post a comment on a user's recent post using it's user name.
+def post_comment(user_name):
+    media_id = get_posts(user_name)
+    url = (BASE_URL + 'media/%s/comments') % (media_id)
+    comment = raw_input("Enter the comment you want to post : ")
+    payload = {"access_token": APP_ACCESS_TOKEN, "text": comment}
+    comment_info = requests.post(url, payload).json()
+    if comment_info["meta"]["code"] is 200:
+        print("Your comment was successfully posted!!")
+    else:
+        print("Response code other than 200 was recieved!!")
 
 
 
